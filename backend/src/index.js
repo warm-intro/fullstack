118713cc619294b11b1ca789e57d7c88e097df1a
zackzeyu <zeyuzhu1@gmail.com>
2021-09-23 12:06:45 -0400

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3030;
+
+app.use(cors());
 
 /**
  * OpenWeather API:
@@ -15,3 +18,26 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server: http://localhost:${port}`);
 });
+
+const exampleLocations = [
+  {
+    name: 'New York, NY',
+    longitude: -73.935242,
+    latitude: 40.73061,
+  },
+  {
+    name: 'New Haven, CT',
+    longitude: -72.929916,
+    latitude: 41.310726,
+  },
+  {
+    name: 'Springfield, IL',
+    longitude: -89.650002,
+    latitude: 39.799999,
+  },
+  {
+    name: 'Austin, TX',
+    longitude: -97.73333,
+    latitude: 30.266666,
+  },
+];
